aa2abcd3c043d5b1aa5dbf7b2823a18736e48c30
xminertools <105097615+xminertools@users.noreply.github.com>
2022-09-17 01:31:42 +0800

    Update linux-install.sh

--- linux-install.sh
+++ linux-install.sh
@@ -2,7 +2,7 @@
 # Author: XMinerTools
 # github: https://github.com/xminertools
 
-VERSION="2.8.6"
+VERSION="2.8.8"
 
 DOWNLOAD_HOST="https://github.com/xminertools/x/raw/main/Linux-64"
 
@@ -22,7 +22,7 @@ PATH_TURN_ON="/etc/profile.d"
 PATH_TURN_ON_SH="/etc/profile.d/XMP.sh"
 
 ISSUE() {
-    echo "2.8.6"
+    echo "2.8.8"
 }
 
 
@@ -157,7 +157,7 @@ start() {
 update() {
     turn_off
 
-    installapp 2.8.6
+    installapp 2.8.8
 }
 
 turn_on() {
@@ -405,7 +405,7 @@ read -p "$(echo -e "请选择[1-14]：")" choose
 
 case $choose in
 1)
-    installapp 2.8.6
+    installapp 2.8.8
     ;;
 2)
     uninstall
